8f024b51ebe27657987d74c6d72c8dbce21d0ad3
Roberto <roberto@austincodingacademy.com>
2017-04-28 22:05:24 +0000

    update

--- create.sh
+++ create.sh
@@ -33,6 +33,7 @@ title: $site
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <title>{{ page.title }}</title>
+    <link rel="stylesheet" href="/css/main.css" type="text/css" />
 </head>
 <body>
     <h1>Hello from Jekyll</h1>
